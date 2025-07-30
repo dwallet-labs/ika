f6c6be40ed8401ba2f1a885f7cedc5f8aad8aba3
Itay <itaylevy134@gmail.com>
2025-07-30 17:36:56 +0300

    good

--- sdk/typescript/test/chaos-test/chaos.test.ts
+++ sdk/typescript/test/chaos-test/chaos.test.ts
@@ -88,30 +88,31 @@ describe('chaos tests', () => {
 
 		const startCommitteeSize = Number(process.env.VALIDATOR_NUM);
 		// ------------ Create Ika Genesis ------------
-		const createIkaGenesisPath = `${TEST_ROOT_DIR}/create-ika-genesis-mac.sh`;
-		await execa({
-			stdout: ['pipe', 'inherit'],
-			stderr: ['pipe', 'inherit'],
-			cwd: TEST_ROOT_DIR,
-		})`${createIkaGenesisPath}`;
-		await fs.copyFile(
-			`${TEST_ROOT_DIR}/${process.env.SUBDOMAIN}/publisher/ika_config.json`,
-			path.resolve(process.cwd(), '../../ika_config.json'),
-		);
+		// const createIkaGenesisPath = `${TEST_ROOT_DIR}/create-ika-genesis-mac.sh`;
+		// await execa({
+		// 	stdout: ['pipe', 'inherit'],
+		// 	stderr: ['pipe', 'inherit'],
+		// 	cwd: TEST_ROOT_DIR,
+		// })`${createIkaGenesisPath}`;
+		// await fs.copyFile(
+		// 	`${TEST_ROOT_DIR}/${process.env.SUBDOMAIN}/publisher/ika_config.json`,
+		// 	path.resolve(process.cwd(), '../../ika_config.json'),
+		// );
+		//
+		// console.log(
+		// 	`Ika genesis created, adding ${numOfValidatorsToAdd} validators to the next committee`,
+		// );
+		// const addValidatorScriptPath = `${TEST_ROOT_DIR}/add-validators-to-next-committee.sh`;
+		// await execa(
+		// 	addValidatorScriptPath,
+		// 	[numOfValidatorsToAdd.toString(), (startCommitteeSize + 1).toString()],
+		// 	{
+		// 		stdout: ['pipe', 'inherit'],
+		// 		stderr: ['pipe', 'inherit'],
+		// 		cwd: TEST_ROOT_DIR,
+		// 	},
+		// );
 
-		console.log(
-			`Ika genesis created, adding ${numOfValidatorsToAdd} validators to the next committee`,
-		);
-		const addValidatorScriptPath = `${TEST_ROOT_DIR}/add-validators-to-next-committee.sh`;
-		await execa(
-			addValidatorScriptPath,
-			[numOfValidatorsToAdd.toString(), (startCommitteeSize + 1).toString()],
-			{
-				stdout: ['pipe', 'inherit'],
-				stderr: ['pipe', 'inherit'],
-				cwd: TEST_ROOT_DIR,
-			},
-		);
 
 		console.log('Validators added to the next committee, deploying ika network');
 		await deployIkaNetwork();
