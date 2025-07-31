adb5c3de59d5f14e0360d356151f7250e210a6c3
Itay <itaylevy134@gmail.com>
2025-07-31 17:32:13 +0300

    create ika genesis works again

--- sdk/typescript/test/chaos-test/create-ika-genesis-mac.sh
+++ sdk/typescript/test/chaos-test/create-ika-genesis-mac.sh
@@ -422,14 +422,11 @@ for tuple in "${VALIDATOR_TUPLES[@]}"; do
     VALIDATOR_DIR="$VALIDATOR_HOSTNAME"
     rm -rf "$SUI_CONFIG_PATH"
     mkdir -p "$SUI_CONFIG_PATH"
-    echo pwd
     cp -r "$VALIDATOR_DIR/$SUI_BACKUP_DIR/sui_config/"* "$SUI_CONFIG_PATH"
 
     echo "Joining committee for Validator '$VALIDATOR_NAME' (Cap ID: $VALIDATOR_CAP_ID)"
     VAL_IKA_CONFIG_DIR="/tmp/ika_config_${VALIDATOR_NAME}"
     IKA_SUI_CONFIG_FILE="$VAL_IKA_CONFIG_DIR/ika_sui_config.yaml"
-    echo "$IKA_SUI_CONFIG_FILE"
-    echo "$VALIDATOR_CAP_ID"
     $BINARY_NAME validator join-committee \
         --validator-cap-id "$VALIDATOR_CAP_ID" --ika-sui-config "$IKA_SUI_CONFIG_FILE"
 done
