d9e1cf529b9b01c7898c00f4439703fbd4bddbd5
Itay <itaylevy134@gmail.com>
2025-08-03 14:25:33 +0300

    start network dkg TX seems fine

--- sdk/typescript/src/dwallet-mpc/network-dkg.ts
+++ sdk/typescript/src/dwallet-mpc/network-dkg.ts
@@ -13,12 +13,7 @@ import {
 
 export async function createNetworkKey(c: Config, protocolCapID: string): Promise<string> {
 	const tx = new Transaction();
-	const emptyIKACoin = tx.moveCall({
-		target: `${SUI_PACKAGE_ID}::coin::zero`,
-		arguments: [],
-		typeArguments: [`${c.ikaConfig.packages.ika_package_id}::ika::IKA`],
-	});
-	const dwalletStateArg = tx.sharedObjectRef({
+	const coordinatorStateArg = tx.sharedObjectRef({
 		objectId: c.ikaConfig.objects.ika_dwallet_coordinator_object_id,
 		initialSharedVersion: await getInitialSharedVersion(
 			c,
@@ -26,19 +21,25 @@ export async function createNetworkKey(c: Config, protocolCapID: string): Promis
 		),
 		mutable: true,
 	});
-	let verifiedProtocolCap = tx.moveCall({
+	const systemStateArg = tx.sharedObjectRef({
+		objectId: c.ikaConfig.objects.ika_dwallet_coordinator_object_id,
+		initialSharedVersion: await getInitialSharedVersion(
+			c,
+			c.ikaConfig.objects.ika_system_object_id,
+		),
+		mutable: true,
+	});
+	const verifiedProtocolCap = tx.moveCall({
 		target: `${c.ikaConfig.packages.ika_system_package_id}::${DWALLET_SYSTEM_MOVE_MODULE_NAME}::verify_protocol_cap`,
-		arguments: [dwalletStateArg, tx.object(protocolCapID)],
+		arguments: [systemStateArg, tx.object(protocolCapID)],
 	});
-	const dwalletCap = tx.moveCall({
-		target: `${c.ikaConfig.packages.ika_dwallet_2pc_mpc_package_id}::${DWALLET_COORDINATOR_MOVE_MODULE_NAME}::request_dwallet_network_encryption_key_dkg_by_cap`,
-		arguments: [dwalletStateArg, tx.pure(bcs.vector(bcs.u8()).serialize([]))],
-	});
-	tx.transferObjects([dwalletCap], c.suiClientKeypair.toSuiAddress());
 	tx.moveCall({
-		target: `${SUI_PACKAGE_ID}::coin::destroy_zero`,
-		arguments: [emptyIKACoin],
-		typeArguments: [`${c.ikaConfig.packages.ika_package_id}::ika::IKA`],
+		target: `${c.ikaConfig.packages.ika_dwallet_2pc_mpc_package_id}::${DWALLET_COORDINATOR_MOVE_MODULE_NAME}::request_dwallet_network_encryption_key_dkg_by_cap`,
+		arguments: [
+			coordinatorStateArg,
+			tx.pure(bcs.vector(bcs.u8()).serialize([])),
+			verifiedProtocolCap,
+		],
 	});
 	const result = await c.client.signAndExecuteTransaction({
 		signer: c.suiClientKeypair,
