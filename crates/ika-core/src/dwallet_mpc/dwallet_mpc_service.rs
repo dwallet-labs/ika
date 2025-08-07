87746f9e8147c9e057a82a8080a2abab2b2c777e
Itay <itaylevy134@gmail.com>
2025-08-07 18:50:46 +0300

    good

--- crates/ika-core/src/dwallet_mpc/dwallet_mpc_service.rs
+++ crates/ika-core/src/dwallet_mpc/dwallet_mpc_service.rs
@@ -1208,6 +1208,9 @@ mod tests {
                     break;
                 }
                 tokio::time::sleep(Duration::from_millis(100)).await;
+                let _ = dwallet_mpc_service
+                    .process_cryptographic_computations()
+                    .await;
             }
         }
     }
