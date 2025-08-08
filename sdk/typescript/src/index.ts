26cd5a3bcbba4601d7f8979ee3c053b011af1994
iamknownasfesal <mkircal957@gmail.com>
2025-08-08 14:29:25 +0300

    feat: index exports

--- sdk/typescript/src/index.ts
+++ sdk/typescript/src/index.ts
@@ -1,4 +1,7 @@
-// Main entry point
+import * as CoordinatorModule from './generated/ika_dwallet_2pc_mpc/coordinator';
+import * as CoordinatorInnerModule from './generated/ika_dwallet_2pc_mpc/coordinator_inner';
+import * as SessionsManagerModule from './generated/ika_dwallet_2pc_mpc/sessions_manager';
+import * as SystemModule from './generated/ika_system/system';
 
 export * as coordinatorTransactions from './tx/coordinator';
 export * as systemTransactions from './tx/system';
@@ -10,3 +13,5 @@ export * from './client/network-configs';
 export * from './client/types';
 export * from './client/user-share-encryption-keys';
 export * from './client/utils';
+
+export { CoordinatorModule, CoordinatorInnerModule, SessionsManagerModule, SystemModule };
