e2a7b8f253d805c0435e823c730991babbd2c788
Itay <itaylevy134@gmail.com>
2025-08-18 16:13:21 +0300

    fix

--- crates/ika-core/src/dwallet_mpc/integration_tests/create_dwallet.rs
+++ crates/ika-core/src/dwallet_mpc/integration_tests/create_dwallet.rs
@@ -128,7 +128,7 @@ async fn create_dwallet() {
         committee,
         sui_data_senders,
     };
-    for mut service in &test_state.dwallet_mpc_services {
+    for service in &mut test_state.dwallet_mpc_services {
         service
             .dwallet_mpc_manager()
             .last_session_to_complete_in_current_epoch = 4;
