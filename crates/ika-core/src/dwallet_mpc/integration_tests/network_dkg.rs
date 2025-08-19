fed60fbfe2faeb5737210dba0ab07733986a1257
Itay <itaylevy134@gmail.com>
2025-08-19 15:45:21 +0300

    working

--- crates/ika-core/src/dwallet_mpc/integration_tests/network_dkg.rs
+++ crates/ika-core/src/dwallet_mpc/integration_tests/network_dkg.rs
@@ -6,6 +6,7 @@
 //! local DB every [`READ_INTERVAL_MS`] seconds
 //! and forward them to the [`DWalletMPCManager`].
 
+use crate::SuiDataSenders;
 use crate::consensus_adapter::SubmitToConsensus;
 use crate::dwallet_checkpoints::PendingDWalletCheckpoint;
 use crate::dwallet_mpc::dwallet_mpc_service::DWalletMPCService;
@@ -19,8 +20,10 @@ use crate::epoch::submit_to_consensus::DWalletMPCSubmitToConsensus;
 use ika_types::committee::Committee;
 use ika_types::message::DWalletCheckpointMessageKind;
 use ika_types::messages_consensus::ConsensusTransactionKind;
+use ika_types::messages_dwallet_mpc::test_helpers::new_dwallet_session_event;
 use ika_types::messages_dwallet_mpc::{
-    DBSuiEvent, DWalletNetworkEncryptionKeyData, DWalletNetworkEncryptionKeyState, IkaNetworkConfig,
+    DBSuiEvent, DWalletDKGFirstRoundRequestEvent, DWalletEncryptionKeyReconfigurationRequestEvent,
+    DWalletNetworkEncryptionKeyData, DWalletNetworkEncryptionKeyState, IkaNetworkConfig,
 };
 use ika_types::messages_dwallet_mpc::{
     DWalletNetworkDKGEncryptionKeyRequestEvent, DWalletSessionEvent, DWalletSessionEventTrait,
@@ -30,7 +33,7 @@ use itertools::Itertools;
 use std::collections::HashMap;
 use std::sync::Arc;
 use std::time::Duration;
-use sui_types::base_types::ObjectID;
+use sui_types::base_types::{EpochId, ObjectID};
 use sui_types::messages_consensus::Round;
 use tracing::{error, info};
 
@@ -59,6 +62,34 @@ async fn test_network_dkg_full_flow() {
         sui_data_senders,
     };
     create_network_key_test(&mut test_state).await;
+}
+
+#[tokio::test]
+#[cfg(test)]
+async fn test_network_key_reconfiguration() {
+    let _ = tracing_subscriber::fmt().with_test_writer().try_init();
+    let (committee, _) = Committee::new_simple_test_committee();
+    let ika_network_config = IkaNetworkConfig::new_for_testing();
+    let epoch_id = 1;
+    let (
+        mut dwallet_mpc_services,
+        mut sui_data_senders,
+        mut sent_consensus_messages_collectors,
+        mut epoch_stores,
+        notify_services,
+    ) = utils::create_dwallet_mpc_services(4);
+    let mut test_state = utils::IntegrationTestState {
+        dwallet_mpc_services,
+        sent_consensus_messages_collectors,
+        epoch_stores,
+        notify_services,
+        crypto_round: 1,
+        consensus_round: 1,
+        committee,
+        sui_data_senders,
+    };
+    let (consensus_round, network_key_bytes, key_id) =
+        create_network_key_test(&mut test_state).await;
 }
 
 pub(crate) async fn create_network_key_test(
@@ -118,4 +149,35 @@ pub(crate) async fn create_network_key_test(
                 )])));
         });
     (consensus_round, network_key_bytes, key_id.unwrap())
+}
+
+pub(crate) fn send_start_network_key_reconfiguration_event(
+    ika_network_config: &IkaNetworkConfig,
+    epoch_id: EpochId,
+    sui_data_senders: &mut Vec<SuiDataSenders>,
+    session_identifier_preimage: [u8; 32],
+    session_sequence_number: u64,
+    dwallet_network_encryption_key_id: ObjectID,
+) {
+    sui_data_senders.iter().for_each(|mut sui_data_sender| {
+        let _ = sui_data_sender.uncompleted_events_sender.send((
+            vec![DBSuiEvent {
+                type_:
+                    DWalletSessionEvent::<DWalletEncryptionKeyReconfigurationRequestEvent>::type_(
+                        &ika_network_config,
+                    ),
+                contents: bcs::to_bytes(&new_dwallet_session_event(
+                    false,
+                    session_sequence_number,
+                    session_identifier_preimage.to_vec().clone(),
+                    DWalletEncryptionKeyReconfigurationRequestEvent {
+                        dwallet_network_encryption_key_id,
+                    },
+                ))
+                .unwrap(),
+                pulled: false,
+            }],
+            epoch_id,
+        ));
+    });
 }
