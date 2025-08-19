db52e68dd7096a49bad4fb7aaaf9fc040191a16c
Itay <itaylevy134@gmail.com>
2025-08-19 18:32:35 +0300

    fix

--- crates/ika-core/src/dwallet_mpc/integration_tests/utils.rs
+++ crates/ika-core/src/dwallet_mpc/integration_tests/utils.rs
@@ -569,7 +569,7 @@ pub(crate) fn send_start_dwallet_dkg_first_round_event(
     sui_data_senders.iter().for_each(|mut sui_data_sender| {
         let _ = sui_data_sender.uncompleted_events_sender.send((
             vec![DWalletSessionRequest {
-                session_type: SessionType::User,
+                session_type: SessionType::System,
                 session_identifier: SessionIdentifier::new(
                     SessionType::System,
                     session_identifier_preimage,
