4232a50becdeda072a8d3045fd8ecb857309ac07
Itay <itaylevy134@gmail.com>
2025-08-19 14:18:41 +0300

    encrypt secret share test

--- crates/ika-core/src/dwallet_mpc/integration_tests/encrypt_secret_share.rs
+++ crates/ika-core/src/dwallet_mpc/integration_tests/encrypt_secret_share.rs
@@ -1,11 +1,17 @@
-use sui_types::base_types::ObjectID;
-use tracing::info;
-use ika_types::committee::Committee;
-use ika_types::messages_dwallet_mpc::IkaNetworkConfig;
 use crate::dwallet_mpc::integration_tests::create_dwallet::create_dwallet_test;
 use crate::dwallet_mpc::integration_tests::network_dkg::create_network_key_test;
 use crate::dwallet_mpc::integration_tests::utils;
-use crate::dwallet_mpc::integration_tests::utils::{send_start_encrypt_secret_share_event, IntegrationTestState};
+use crate::dwallet_mpc::integration_tests::utils::{
+    IntegrationTestState, send_start_encrypt_secret_share_event,
+};
+use dwallet_mpc_centralized_party::{
+    encrypt_secret_key_share_and_prove, network_dkg_public_output_to_protocol_pp_inner,
+};
+use ika_types::committee::Committee;
+use ika_types::message::DWalletCheckpointMessageKind;
+use ika_types::messages_dwallet_mpc::IkaNetworkConfig;
+use sui_types::base_types::ObjectID;
+use tracing::info;
 
 #[tokio::test]
 #[cfg(test)]
@@ -38,8 +44,20 @@ async fn encrypt_secret_share() {
     }
     let (consensus_round, network_key_bytes, key_id) =
         create_network_key_test(&mut test_state).await;
-    let (consensus_round, dwallet_dkg_second_round_output, secret_share) =
-        create_dwallet_test(&mut test_state, consensus_round, key_id, network_key_bytes).await;
+    let dwallet_test_result = create_dwallet_test(
+        &mut test_state,
+        consensus_round,
+        key_id,
+        network_key_bytes.clone(),
+    )
+    .await;
+    let protocol_pp = network_dkg_public_output_to_protocol_pp_inner(network_key_bytes).unwrap();
+    let encrypted_secret_share = encrypt_secret_key_share_and_prove(
+        dwallet_test_result.dwallet_secret_key_share.clone(),
+        dwallet_test_result.class_groups_encryption_key.clone(),
+        protocol_pp,
+    )
+    .unwrap();
     send_start_encrypt_secret_share_event(
         &ika_network_config,
         epoch_id,
@@ -47,10 +65,32 @@ async fn encrypt_secret_share() {
         [4; 32],
         4,
         key_id,
-        ObjectID::from_bytes(dwallet_dkg_second_round_output.dwallet_id.clone()).unwrap(),
-        todo!(),
-        dwallet_dkg_second_round_output.output,
-        
+        ObjectID::from_bytes(
+            dwallet_test_result
+                .dkg_second_round_output
+                .dwallet_id
+                .clone(),
+        )
+        .unwrap(),
+        encrypted_secret_share,
+        dwallet_test_result.dkg_second_round_output.output,
+        dwallet_test_result.class_groups_encryption_key.clone(),
     );
-    info!("DWallet DKG second round completed");
+    let (consensus_round, encrypted_secret_share_checkpoint) =
+        utils::advance_mpc_flow_until_completion(&mut test_state, consensus_round).await;
+    let DWalletCheckpointMessageKind::RespondDWalletEncryptedUserShare(
+        encrypted_secret_share_output,
+    ) = encrypted_secret_share_checkpoint
+        .messages()
+        .clone()
+        .pop()
+        .unwrap()
+    else {
+        panic!("Expected DWallet encrypted secret share output message");
+    };
+    assert!(
+        !encrypted_secret_share_output.rejected,
+        "Encrypted secret share was rejected"
+    );
+    info!("Encrypted secret share flow completed successfully");
 }
