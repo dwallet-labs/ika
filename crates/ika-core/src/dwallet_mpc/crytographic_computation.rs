d591c076c908c225361f775dba9bbd1b26c01cc6
Yael Abergel <yaelab200@gmail.com>
2025-08-19 16:29:36 +0300

    --wip-- [skip ci]

--- crates/ika-core/src/dwallet_mpc/crytographic_computation.rs
+++ crates/ika-core/src/dwallet_mpc/crytographic_computation.rs
@@ -17,11 +17,6 @@ const MPC_SIGN_SECOND_ROUND: u64 = 2;
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub(crate) struct ComputationId {
     pub(crate) session_identifier: SessionIdentifier,
-    /// The consensus round at which this computation executed (if it is synced with the consensus).
-    /// The first MPC round will be `None`, since it isn't synced — it is launched when the
-    /// event is received from Sui.
-    /// All other MPC rounds will set this to `Some()` with the value being the last consensus
-    /// round from which we gathered messages to advance.
     pub(crate) consensus_round: u64,
     pub(crate) mpc_round: u64,
     pub(crate) attempt_number: u64,
