030df4c0fa27a0e9e966f4c0edf5641e751af3db
Itay <itaylevy134@gmail.com>
2025-08-21 12:22:39 +0300

    good

--- crates/ika-core/src/dwallet_mpc/integration_tests/create_dwallet.rs
+++ crates/ika-core/src/dwallet_mpc/integration_tests/create_dwallet.rs
@@ -100,7 +100,6 @@ async fn dwallet_dkg_first_round() {
 
 #[tokio::test]
 #[cfg(test)]
-/// Runs a network DKG and then uses the resulting network key to run the DWallet DKG first round.
 async fn create_dwallet() {
     let _ = tracing_subscriber::fmt().with_test_writer().try_init();
     let (committee, _) = Committee::new_simple_test_committee();
