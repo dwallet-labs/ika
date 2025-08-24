6b1b2bb2077e0eec7e8504fb67a1f551cf224c6f
Itay Levy <itaylevy134@gmail.com>
2025-08-24 11:33:22 +0300

    fix: Stop the chain from printing junk `no available cores` logs (#1454)

--- crates/ika-core/src/dwallet_mpc/crytographic_computation.rs
+++ crates/ika-core/src/dwallet_mpc/crytographic_computation.rs
@@ -7,17 +7,29 @@ mod orchestrator;
 pub mod protocol_cryptographic_data;
 mod request;
 
+use derivative::Derivative;
 use ika_types::messages_dwallet_mpc::SessionIdentifier;
 pub(crate) use orchestrator::CryptographicComputationsOrchestrator;
 pub(crate) use request::Request as ComputationRequest;
+use std::hash::Hash;
 
 const MPC_SIGN_SECOND_ROUND: u64 = 2;
 
 /// A unique key for a computation request.
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+#[derive(Derivative)]
+#[derivative(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub(crate) struct ComputationId {
     pub(crate) session_identifier: SessionIdentifier,
-    pub(crate) consensus_round: u64,
     pub(crate) mpc_round: u64,
     pub(crate) attempt_number: u64,
+
+    /// Do not include the consensus round in the equality check. A new computation is created
+    /// every few consensus rounds when
+    /// [`crate::dwallet_mpc::mpc_manager::DWalletMPCManager::perform_cryptographic_computation`]
+    /// is called. Then, the chain checks if this computation has already been spawned.
+    /// If the consensus round were part of the equality check, the chain would always treat it
+    /// as a new computation and spawn one unnecessarily.
+    #[derivative(Hash = "ignore")]
+    #[derivative(PartialEq = "ignore")]
+    pub(crate) consensus_round: u64,
 }
