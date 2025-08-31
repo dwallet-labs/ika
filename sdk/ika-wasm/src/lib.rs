71b22d3cfe1a25aceb871486a3ed86842c59ed93
Itay <itaylevy134@gmail.com>
2025-08-31 15:51:11 +0300

    dmt

--- sdk/ika-wasm/src/lib.rs
+++ sdk/ika-wasm/src/lib.rs
@@ -17,11 +17,9 @@ pub fn create_dkg_centralized_output_v1(
     protocol_pp: Vec<u8>,
     decentralized_first_round_public_output: Vec<u8>,
 ) -> Result<JsValue, JsError> {
-    let dkg_centralized_result = &create_dkg_output_v1(
-        protocol_pp,
-        decentralized_first_round_public_output,
-    )
-    .map_err(|e| JsError::new(&e.to_string()))?;
+    let dkg_centralized_result =
+        &create_dkg_output_v1(protocol_pp, decentralized_first_round_public_output)
+            .map_err(|e| JsError::new(&e.to_string()))?;
     serde_wasm_bindgen::to_value(&(
         dkg_centralized_result.public_key_share_and_proof.clone(),
         dkg_centralized_result.public_output.clone(),
