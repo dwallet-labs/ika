75a1483f14849ba0e31cb455495915f181fb5873
Itay <itaylevy134@gmail.com>
2025-09-02 18:15:54 +0300

    advanceandfinalize

--- crates/ika-core/src/dwallet_mpc/crytographic_computation/mpc_computations.rs
+++ crates/ika-core/src/dwallet_mpc/crytographic_computation/mpc_computations.rs
@@ -661,18 +661,9 @@ impl ProtocolCryptographicData {
                         private_output,
                     } => {
                         // Wrap the public output with its version.
-                        let public_output_value = match protocol_config
-                            .network_encryption_key_version
-                        {
-                            Some(2) => {
-                                bcs::to_bytes(&VersionedDecryptionKeyReconfigurationOutput::V2(
-                                    public_output_value,
-                                ))?
-                            }
-                            _ => bcs::to_bytes(&VersionedDecryptionKeyReconfigurationOutput::V1(
-                                public_output_value,
-                            ))?,
-                        };
+                        let public_output_value = bcs::to_bytes(
+                            &VersionedDecryptionKeyReconfigurationOutput::V1(public_output_value),
+                        )?;
 
                         Ok(GuaranteedOutputDeliveryRoundResult::Finalize {
                             public_output_value,
@@ -695,6 +686,50 @@ impl ProtocolCryptographicData {
 
                 let result =
                     Party::<ReconfigurationV1toV2Secp256k1Party>::advance_with_guaranteed_output(
+                        session_id,
+                        party_id,
+                        access_structure,
+                        advance_request,
+                        Some(decryption_key_shares.clone()),
+                        &public_input,
+                        &mut rng,
+                    )?;
+
+                match result {
+                    GuaranteedOutputDeliveryRoundResult::Advance { message } => {
+                        Ok(GuaranteedOutputDeliveryRoundResult::Advance { message })
+                    }
+                    GuaranteedOutputDeliveryRoundResult::Finalize {
+                        public_output_value,
+                        malicious_parties,
+                        private_output,
+                    } => {
+                        // Wrap the public output with its version.
+                        let public_output_value = bcs::to_bytes(
+                            &VersionedDecryptionKeyReconfigurationOutput::V2(public_output_value),
+                        )?;
+
+                        Ok(GuaranteedOutputDeliveryRoundResult::Finalize {
+                            public_output_value,
+                            malicious_parties,
+                            private_output,
+                        })
+                    }
+                }
+            }
+            ProtocolCryptographicData::NetworkEncryptionKeyV2Reconfiguration {
+                public_input,
+                advance_request,
+                decryption_key_shares,
+                ..
+            } => {
+                let decryption_key_shares = decryption_key_shares
+                    .iter()
+                    .map(|(party_id, share)| (*party_id, share.decryption_key_share))
+                    .collect::<HashMap<_, _>>();
+
+                let result =
+                    Party::<ReconfigurationV2Secp256k1Party>::advance_with_guaranteed_output(
                         session_id,
                         party_id,
                         access_structure,
