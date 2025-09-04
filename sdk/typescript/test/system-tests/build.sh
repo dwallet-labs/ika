ec3a2ed20d670bc4a048464f6522b11d9ce089a4
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-09-04 20:53:12 +0300

    wip

--- sdk/typescript/test/system-tests/build.sh
+++ sdk/typescript/test/system-tests/build.sh
@@ -44,6 +44,5 @@ echo
 docker build -f "$DOCKERFILE" "$REPO_ROOT" \
   --build-arg GIT_REVISION="$GIT_REVISION" \
   --build-arg BUILD_DATE="$BUILD_DATE" \
-  --build-arg WITH_NETWORK_DKG="$WITH_NETWORK_DKG" \
   --build-arg CARGO_BUILD_FLAGS="$1" \
   --tag "$DOCKER_TAG"
