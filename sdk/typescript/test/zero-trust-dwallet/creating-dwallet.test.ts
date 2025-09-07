277f8ee91b4acc76e63019c65ca4177bff97563b
Itay <itaylevy134@gmail.com>
2025-09-07 16:02:28 +0300

    comment

--- sdk/typescript/test/zero-trust-dwallet/creating-dwallet.test.ts
+++ sdk/typescript/test/zero-trust-dwallet/creating-dwallet.test.ts
@@ -197,6 +197,7 @@ describe('DWallet Creation', () => {
 		);
 		const sessionIdentifier = createSessionIDIkaTx.createSessionIdentifier();
 		createSessionIDTx.transferObjects([sessionIdentifier], signerAddress);
+		// Sleep for a bit to free the signer keypair gas object
 		await delay(5);
 		const registerSessionIDResult = await executeTestTransaction(
 			suiClient,
