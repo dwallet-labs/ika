9ad38670bafe20e11495be1ee397541735335fe8
Yael Abergel <yaelab200@gmail.com>
2025-09-09 11:59:55 +0300

    --wip-- [skip ci]

--- crates/ika-core/src/dwallet_mpc/mpc_session/input.rs
+++ crates/ika-core/src/dwallet_mpc/mpc_session/input.rs
@@ -20,7 +20,9 @@ use crate::dwallet_session_request::DWalletSessionRequest;
 use crate::request_protocol_data::ProtocolData;
 use class_groups::dkg;
 use commitment::CommitmentSizedNumber;
-use dwallet_mpc_types::dwallet_mpc::{MPCPrivateInput, VersionedImportedDWalletPublicOutput};
+use dwallet_mpc_types::dwallet_mpc::{
+    DWalletCurve, MPCPrivateInput, VersionedImportedDWalletPublicOutput,
+};
 use group::PartyID;
 use ika_protocol_config::ProtocolConfig;
 use ika_types::committee::{ClassGroupsEncryptionKeyAndProof, Committee};
