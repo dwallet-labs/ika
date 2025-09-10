a0f0078fc137f45101c40724162a2c218f9d9709
Itay <itaylevy134@gmail.com>
2025-09-10 16:09:55 +0300

    upgrade script seems chill

--- sdk/typescript/test/move-upgrade/upgrade-ika-twopc-mpc.ts
+++ sdk/typescript/test/move-upgrade/upgrade-ika-twopc-mpc.ts
@@ -10,87 +10,69 @@ import { fromBase64 } from '@mysten/sui/utils';
 
 import { createTestIkaClient } from '../helpers/test-utils';
 
-const SUI = 'sui';
-const CAP_ID = '<EXAMPLE-UPGRADE-CAP>';
-const sender = execSync(`${SUI} client active-address`, { encoding: 'utf8' }).trim();
-const signer = (() => {
-	const keystore = JSON.parse(
-		readFileSync(path.join(homedir(), '.sui', 'sui_config', 'sui.keystore'), 'utf8'),
+const packagePath = '/root/code/dwallet-network/contracts/ika_dwallet_2pc_mpc';
+
+async function main() {
+	let signer = Ed25519Keypair.deriveKeypair('<PUBLISHER-KEYPAIR>');
+	const { modules, dependencies, digest } = JSON.parse(
+		execSync(`sui move build --dump-bytecode-as-base64 --path ${packagePath}`, {
+			encoding: 'utf-8',
+		}),
 	);
 
-	for (const priv of keystore) {
-		const raw = fromBase64(priv);
-		if (raw[0] !== 0) {
-			continue;
-		}
+	const tx = new Transaction();
+	const protocolCap = tx.object('<PROTOCOL-CAP-ID>');
+	const suiClient = new SuiClient({ url: getFullnodeUrl('localnet') });
+	const ikaClient = createTestIkaClient(suiClient);
+	const systemStateArg = tx.sharedObjectRef({
+		objectId: ikaClient.ikaConfig.objects.ikaSystemObject.objectID,
+		initialSharedVersion: ikaClient.ikaConfig.objects.ikaSystemObject.initialSharedVersion,
+		mutable: false,
+	});
 
-		const pair = Ed25519Keypair.fromSecretKey(raw.slice(1));
-		if (pair.getPublicKey().toSuiAddress() === sender) {
-			return pair;
-		}
-	}
+	tx.moveCall({
+		target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::set_approved_upgrade_by_cap`,
+		arguments: [
+			systemStateArg,
+			tx.pure.id(ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage),
+			tx.pure(digest),
+			protocolCap,
+		],
+	});
 
-	throw new Error(`key pair not found for sender: ${sender}`);
-})();
+	let [upgradeTicket, upgradeApprover] = tx.moveCall({
+		target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::authorize_upgrade`,
+		arguments: [systemStateArg, tx.pure.id(ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage)],
+	});
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const packagePath = path.join(__dirname, 'example');
+	const receipt = tx.upgrade({
+		modules,
+		dependencies,
+		package: ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage,
+		ticket: upgradeTicket,
+	});
 
-const { modules, dependencies, digest } = JSON.parse(
-	execSync(`${SUI} move build --dump-bytecode-as-base64 --path ${packagePath}`, {
-		encoding: 'utf-8',
-	}),
-);
+	tx.moveCall({
+		target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::commit_upgrade`,
+		arguments: [systemStateArg, receipt, upgradeApprover],
+	});
 
-const tx = new Transaction();
-const protocolCap = tx.object('<PROTOCOL-CAP-ID>');
-const suiClient = new SuiClient({ url: getFullnodeUrl('localnet') });
-const ikaClient = createTestIkaClient(suiClient);
-const systemStateArg = tx.sharedObjectRef({
-	objectId: ikaClient.ikaConfig.objects.ikaSystemObject.objectID,
-	initialSharedVersion: ikaClient.ikaConfig.objects.ikaSystemObject.initialSharedVersion,
-	mutable: false,
-});
+	const client = new SuiClient({ url: getFullnodeUrl('localnet') });
+	const result = await client.signAndExecuteTransaction({
+		signer,
+		transaction: tx,
+		options: {
+			showEffects: true,
+			showObjectChanges: true,
+		},
+	});
 
-tx.moveCall({
-	target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::set_approved_upgrade_by_cap`,
-	arguments: [
-		systemStateArg,
-		tx.pure.id(ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage),
-		tx.pure(digest),
-		protocolCap,
-	],
-});
+	console.log(result);
+}
 
-let [upgradeTicket, upgradeApprover] = tx.moveCall({
-	target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::authorize_upgrade`,
-	arguments: [
-		systemStateArg,
-		tx.pure.id(ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage),
-	],
-});
-
-const receipt = tx.upgrade({
-	modules,
-	dependencies,
-	package: ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage,
-	ticket: upgradeTicket,
-});
-
-
-tx.moveCall({
-	target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::commit_upgrade`,
-	arguments: [systemStateArg, receipt, upgradeApprover],
-});
-
-const client = new SuiClient({ url: getFullnodeUrl('localnet') });
-const result = await client.signAndExecuteTransaction({
-	signer,
-	transaction: tx,
-	options: {
-		showEffects: true,
-		showObjectChanges: true,
-	},
-});
-
-console.log(result);
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.then(() => process.exit(0));
