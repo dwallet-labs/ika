01ae2f8d33586edf8f7b2a4249af1ce81e5c97ce
Itay <itaylevy134@gmail.com>
2025-09-10 17:03:53 +0300

    values

--- sdk/typescript/test/move-upgrade/upgrade-ika-twopc-mpc.ts
+++ sdk/typescript/test/move-upgrade/upgrade-ika-twopc-mpc.ts
@@ -8,7 +8,7 @@ import { createTestIkaClient } from '../helpers/test-utils';
 const packagePath = '/root/code/dwallet-network/contracts/ika_dwallet_2pc_mpc';
 
 async function main() {
-	let signer = Ed25519Keypair.deriveKeypair('<PUBLISHER-KEYPAIR>');
+	let signer = Ed25519Keypair.deriveKeypair('walnut near write remember shrimp clarify veteran drastic antique blade barrel flash');
 	const { modules, dependencies, digest } = JSON.parse(
 		execSync(`sui move build --dump-bytecode-as-base64 --path ${packagePath}`, {
 			encoding: 'utf-8',
@@ -16,7 +16,7 @@ async function main() {
 	);
 
 	const tx = new Transaction();
-	const protocolCap = tx.object('<PROTOCOL-CAP-ID>');
+	const protocolCap = tx.object('0x0764674039bfa734701bd4075fb51cffc0a0eec074afcceb5f47ae9f732b5a43');
 	const suiClient = new SuiClient({ url: getFullnodeUrl('localnet') });
 	const ikaClient = createTestIkaClient(suiClient);
 	const systemStateArg = tx.sharedObjectRef({
