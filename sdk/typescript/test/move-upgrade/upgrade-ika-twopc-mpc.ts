be86507de60d137c8bafcaada9cb23767c8ce03b
Itay <itaylevy134@gmail.com>
2025-09-11 11:50:32 +0300

    migrateworked

--- sdk/typescript/test/move-upgrade/upgrade-ika-twopc-mpc.ts
+++ sdk/typescript/test/move-upgrade/upgrade-ika-twopc-mpc.ts
@@ -22,95 +22,100 @@ export async function updateIkaCoordinator() {
 	);
 
 	const tx = new Transaction();
-	const protocolCap = tx.object(
+	let protocolCap = tx.object(
 		'0x67cfbdf4d56c2bea5746dd523e2d8fabd885e3e090e8da5400b86f7c316430c3',
 	);
 	const suiClient = new SuiClient({ url: getFullnodeUrl('localnet') });
 	const ikaClient = createTestIkaClient(suiClient);
 	await ikaClient.initialize();
-	let systemStateArg = tx.sharedObjectRef({
-		objectId: ikaClient.ikaConfig.objects.ikaSystemObject.objectID,
-		initialSharedVersion: ikaClient.ikaConfig.objects.ikaSystemObject.initialSharedVersion,
-		mutable: true,
-	});
-	let coordinatorStateArg = tx.sharedObjectRef({
-		objectId: ikaClient.ikaConfig.objects.ikaDWalletCoordinator.objectID,
-		initialSharedVersion: ikaClient.ikaConfig.objects.ikaDWalletCoordinator.initialSharedVersion,
-		mutable: true,
-	});
-
-	tx.moveCall({
-		target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::set_approved_upgrade_by_cap`,
-		arguments: [
-			systemStateArg,
-			tx.pure.id(ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage),
-			tx.pure(bcs.option(bcs.vector(bcs.u8())).serialize(digest)),
-			protocolCap,
-		],
-	});
-
-	let [upgradeTicket, upgradeApprover] = tx.moveCall({
-		target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::authorize_upgrade`,
-		arguments: [systemStateArg, tx.pure.id(ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage)],
-	});
-
-	const receipt = tx.upgrade({
-		modules,
-		dependencies,
-		package: ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage,
-		ticket: upgradeTicket,
-	});
-
-	tx.moveCall({
-		target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::commit_upgrade`,
-		arguments: [systemStateArg, receipt, upgradeApprover],
-	});
-	tx.moveCall({
-		target: `${ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage}::coordinator::commit_upgrade`,
-		arguments: [coordinatorStateArg, upgradeApprover],
-	});
-
-	tx.moveCall({
-		target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::finalize_upgrade`,
-		arguments: [systemStateArg, upgradeApprover],
-	});
-	const client = new SuiClient({ url: getFullnodeUrl('localnet') });
-	const result = await client.signAndExecuteTransaction({
-		signer,
-		transaction: tx,
-		options: {
-			showEffects: true,
-			showObjectChanges: true,
-
-		},
-	});
-
-	console.log(result);
-	systemStateArg = tx.sharedObjectRef({
-		objectId: ikaClient.ikaConfig.objects.ikaSystemObject.objectID,
-		initialSharedVersion: ikaClient.ikaConfig.objects.ikaSystemObject.initialSharedVersion,
-		mutable: true,
-	});
-
-	coordinatorStateArg = tx.sharedObjectRef({
-		objectId: ikaClient.ikaConfig.objects.ikaDWalletCoordinator.objectID,
-		initialSharedVersion: ikaClient.ikaConfig.objects.ikaDWalletCoordinator.initialSharedVersion,
-		mutable: true,
-	});
-
+	// let systemStateArg = tx.sharedObjectRef({
+	// 	objectId: ikaClient.ikaConfig.objects.ikaSystemObject.objectID,
+	// 	initialSharedVersion: ikaClient.ikaConfig.objects.ikaSystemObject.initialSharedVersion,
+	// 	mutable: true,
+	// });
+	// let coordinatorStateArg = tx.sharedObjectRef({
+	// 	objectId: ikaClient.ikaConfig.objects.ikaDWalletCoordinator.objectID,
+	// 	initialSharedVersion: ikaClient.ikaConfig.objects.ikaDWalletCoordinator.initialSharedVersion,
+	// 	mutable: true,
+	// });
+	//
+	// tx.moveCall({
+	// 	target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::set_approved_upgrade_by_cap`,
+	// 	arguments: [
+	// 		systemStateArg,
+	// 		tx.pure.id(ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage),
+	// 		tx.pure(bcs.option(bcs.vector(bcs.u8())).serialize(digest)),
+	// 		protocolCap,
+	// 	],
+	// });
+	//
+	// let [upgradeTicket, upgradeApprover] = tx.moveCall({
+	// 	target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::authorize_upgrade`,
+	// 	arguments: [systemStateArg, tx.pure.id(ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage)],
+	// });
+	//
+	// const receipt = tx.upgrade({
+	// 	modules,
+	// 	dependencies,
+	// 	package: ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage,
+	// 	ticket: upgradeTicket,
+	// });
+	//
+	// tx.moveCall({
+	// 	target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::commit_upgrade`,
+	// 	arguments: [systemStateArg, receipt, upgradeApprover],
+	// });
+	// tx.moveCall({
+	// 	target: `${ikaClient.ikaConfig.packages.ikaDwallet2pcMpcPackage}::coordinator::commit_upgrade`,
+	// 	arguments: [coordinatorStateArg, upgradeApprover],
+	// });
+	//
+	// tx.moveCall({
+	// 	target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::finalize_upgrade`,
+	// 	arguments: [systemStateArg, upgradeApprover],
+	// });
+	// const client = new SuiClient({ url: getFullnodeUrl('localnet') });
+	// const result = await client.signAndExecuteTransaction({
+	// 	signer,
+	// 	transaction: tx,
+	// 	options: {
+	// 		showEffects: true,
+	// 		showObjectChanges: true,
+	//
+	// 	},
+	// });
+	//
+	// console.log(result);
 	const migrateTx = new Transaction();
+
+	let systemStateArg = migrateTx.sharedObjectRef({
+		objectId: ikaClient.ikaConfig.objects.ikaSystemObject.objectID,
+		initialSharedVersion: ikaClient.ikaConfig.objects.ikaSystemObject.initialSharedVersion,
+		mutable: true,
+	});
+
+	let coordinatorStateArg = migrateTx.sharedObjectRef({
+		objectId: ikaClient.ikaConfig.objects.ikaDWalletCoordinator.objectID,
+		initialSharedVersion: ikaClient.ikaConfig.objects.ikaDWalletCoordinator.initialSharedVersion,
+		mutable: true,
+	});
+	protocolCap = migrateTx.object('0x67cfbdf4d56c2bea5746dd523e2d8fabd885e3e090e8da5400b86f7c316430c3');
 	let verifiedProtocolCap = migrateTx.moveCall({
 		target: `${ikaClient.ikaConfig.packages.ikaSystemPackage}::system::verify_protocol_cap`,
 		arguments: [systemStateArg, protocolCap],
 	});
 
+	// migrateTx.moveCall({
+	// 	target: `${result.effects.created.at(0).reference.objectId}::coordinator::try_migrate_by_cap`,
+	// 	arguments: [coordinatorStateArg, verifiedProtocolCap],
+	// });
 	migrateTx.moveCall({
-		target: `${result.effects.created.at(0).reference.objectId}::coordinator::try_migrate_by_cap`,
+		target: `0x12b76afe3db6b2ed0881f7a22b4ad5f5d58a2bdf8dcf3770b9f86d746a4d3dad::coordinator::try_migrate_by_cap`,
 		arguments: [coordinatorStateArg, verifiedProtocolCap],
 	});
-	const migrateResult = await client.signAndExecuteTransaction({
+	const migrateResult = await suiClient.signAndExecuteTransaction({
 		signer,
-		transaction: tx,
+		transaction: migrateTx,
 		options: {
 			showEffects: true,
 			showObjectChanges: true,
