1b71f2096b12757a3afa4ab77e9221d7f4cc97b9
Itay <itaylevy134@gmail.com>
2025-09-14 13:56:27 +0300

    progress

--- sdk/typescript/test/system-tests/upgrade-network-key/upgrade-network-key.test.ts
+++ sdk/typescript/test/system-tests/upgrade-network-key/upgrade-network-key.test.ts
@@ -1,14 +1,17 @@
 import fs from 'fs';
 import path from 'path';
+import { network_key_version } from '@ika.xyz/ika-wasm';
+import { KubeConfig } from '@kubernetes/client-node';
 import { execa } from 'execa';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 import {
 	createTestIkaClient,
-	createTestSuiClient,
+	createTestSuiClient, delay,
 	waitForEpochSwitch,
 } from '../../helpers/test-utils';
-import { deployIkaNetwork, TEST_ROOT_DIR } from '../globals';
+import { deployIkaNetwork, NAMESPACE_NAME, TEST_ROOT_DIR } from '../globals';
+import { createValidatorPod, killValidatorPod } from '../pods';
 
 describe('system tests', () => {
 	it('run a full flow test of upgrading the network key version and the move code', async () => {
@@ -37,8 +40,24 @@ describe('system tests', () => {
 		console.log('Epoch switched, verifying the network key version is V1');
 		const networkKey = await ikaClient.getConfiguredNetworkEncryptionKey();
 		let networkKeyBytes = await ikaClient.readTableVecAsRawBytes(networkKey.publicOutputID);
-		let networkKeyVersion = networkKeyBytes[0];
-		expect(networkKeyVersion).toBe(0);
+		const networkKeyVersion = network_key_version(networkKeyBytes);
+		expect(networkKeyVersion).toBe(1);
+		console.log('Network key version is V1, upgrading validators binaries to V2');
+		process.env.DOCKER_TAG = 'us-docker.pkg.dev/common-449616/ika-common-containers/ika-node:v2key';
+		const kc = new KubeConfig();
+		kc.loadFromDefault();
+		for (let i = 0; i < Number(process.env.VALIDATOR_NUM); i++) {
+			await killValidatorPod(kc, NAMESPACE_NAME, i + 1);
+			await delay(1);
+			await createValidatorPod(kc, NAMESPACE_NAME, i + 1);
+		}
+		console.log('All validators upgraded, waiting for two epoch switches (the protocol version may have been changed after the epoch middle)');
+		await waitForEpochSwitch(ikaClient);
+		await waitForEpochSwitch(ikaClient);
+		console.log('Two epochs switched, verifying the network key version is V2');
+		networkKeyBytes = await ikaClient.readTableVecAsRawBytes(networkKey.publicOutputID);
+		const newNetworkKeyVersion = network_key_version(networkKeyBytes);
+		expect(newNetworkKeyVersion).toBe(2);
 
 		// console.log('Ika network deployed, waiting for epoch switch');
 	}, 3_600_000);
