730f4910923f057ff63dc5fdc0963b1e51719400
Itay <itaylevy134@gmail.com>
2025-09-29 10:42:38 +0300

    bien

--- sdk/typescript/src/client/ika-client.ts
+++ sdk/typescript/src/client/ika-client.ts
@@ -681,15 +681,14 @@ export class IkaClient {
 			}
 		}
 
-		const protocolPublicParameters =
-			networkEncryptionKey.epoch === Number(objects.coordinatorInner.current_epoch)
-				? await networkDkgPublicOutputToProtocolPublicParameters(
-						await this.#readTableVecAsRawBytes(networkEncryptionKeyPublicOutputID),
-					)
-				: await reconfigurationPublicOutputToProtocolPublicParameters(
-						await this.#readTableVecAsRawBytes(networkEncryptionKey.reconfigurationOutputID!),
-						await this.#readTableVecAsRawBytes(networkEncryptionKeyPublicOutputID),
-					);
+		const protocolPublicParameters = !networkEncryptionKey.reconfigurationOutputID
+			? await networkDkgPublicOutputToProtocolPublicParameters(
+					await this.#readTableVecAsRawBytes(networkEncryptionKeyPublicOutputID),
+				)
+			: await reconfigurationPublicOutputToProtocolPublicParameters(
+					await this.#readTableVecAsRawBytes(networkEncryptionKey.reconfigurationOutputID),
+					await this.#readTableVecAsRawBytes(networkEncryptionKeyPublicOutputID),
+				);
 
 		// Cache the parameters by encryption key ID
 		this.cachedProtocolPublicParameters.set(encryptionKeyID, {
