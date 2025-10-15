0e72d248873bcafc93b386dcb1ff616cb86f8aca
Itay <itaylevy134@gmail.com>
2025-10-15 14:19:37 +0300

    bien

--- sdk/typescript/src/tx/coordinator.ts
+++ sdk/typescript/src/tx/coordinator.ts
@@ -176,7 +176,10 @@ export function requestDWalletDKG(
 			arguments: [signDuringDKGRequest],
 		});
 	} else {
-		signDuringDKGRequestSerialized = tx.pure(bcs.option(SignDuringDKGRequest).serialize(null));
+		signDuringDKGRequestSerialized = tx.object.option({
+			type: `${ikaConfig.packages.ikaDwallet2pcMpcPackage}::coordinator_inner::SignDuringDKGRequest`,
+			value: null,
+		});
 	}
 
 	return tx.moveCall({
