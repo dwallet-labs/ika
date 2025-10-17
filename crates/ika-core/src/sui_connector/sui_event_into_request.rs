ae9624318e0549814a878d5e392dd848aaf889f8
Itay <itaylevy134@gmail.com>
2025-10-17 13:32:05 +0300

    --wip-- [skip ci]

--- crates/ika-core/src/sui_connector/sui_event_into_request.rs
+++ crates/ika-core/src/sui_connector/sui_event_into_request.rs
@@ -7,15 +7,7 @@ use crate::request_protocol_data::{
     partial_signature_verification_protocol_data, presign_protocol_data, sign_protocol_data,
 };
 use ika_types::dwallet_mpc_error::DwalletMPCResult;
-use ika_types::messages_dwallet_mpc::{
-    DWALLET_SESSION_EVENT_STRUCT_NAME, DWalletDKGFirstRoundRequestEvent, DWalletDKGRequestEvent,
-    DWalletDKGSecondRoundRequestEvent, DWalletEncryptionKeyReconfigurationRequestEvent,
-    DWalletImportedKeyVerificationRequestEvent, DWalletNetworkDKGEncryptionKeyRequestEvent,
-    DWalletSessionEvent, DWalletSessionEventTrait, EncryptedShareVerificationRequestEvent,
-    FutureSignRequestEvent, IkaNetworkConfig, MakeDWalletUserSecretKeySharesPublicRequestEvent,
-    PresignRequestEvent, SESSIONS_MANAGER_MODULE_NAME, SignRequestEvent,
-    UserSecretKeyShareEventType,
-};
+use ika_types::messages_dwallet_mpc::{DWALLET_SESSION_EVENT_STRUCT_NAME, DWalletDKGFirstRoundRequestEvent, DWalletDKGRequestEvent, DWalletDKGSecondRoundRequestEvent, DWalletEncryptionKeyReconfigurationRequestEvent, DWalletImportedKeyVerificationRequestEvent, DWalletNetworkDKGEncryptionKeyRequestEvent, DWalletSessionEvent, DWalletSessionEventTrait, EncryptedShareVerificationRequestEvent, FutureSignRequestEvent, IkaNetworkConfig, MakeDWalletUserSecretKeySharesPublicRequestEvent, PresignRequestEvent, SESSIONS_MANAGER_MODULE_NAME, SignRequestEvent, UserSecretKeyShareEventType, SignDuringDKGRequestEvent};
 use move_core_types::language_storage::StructTag;
 use serde::de::DeserializeOwned;
 use sui_types::dynamic_field::Field;
@@ -93,10 +85,22 @@ pub fn sui_event_into_session_request(
             .name
             .to_string(),
     ) {
-        dwallet_dkg_session_request(
-            deserialize_event_contents::<DWalletDKGRequestEvent>(&contents, pulled)?,
-            pulled,
-        )?
+        let parsed_event = deserialize_event_contents::<DWalletDKGRequestEvent>(&contents, pulled)?;
+        match &parsed_event.event_data.sign_during_dkg_request {
+            None => {
+                dwallet_dkg_session_request(
+                    parsed_event,
+                    pulled,
+                )?
+            }
+            Some(sign_during_dkg_request) => {
+                dwallet_dkg_with_sign_session_request(
+                    parsed_event,
+                    pulled,
+                    sign_during_dkg_request
+                )?
+            }
+        }
     } else if event_type.to_string().contains(
         &DWalletDKGSecondRoundRequestEvent::type_(packages_config)
             .name
@@ -208,6 +212,26 @@ fn dwallet_imported_key_verification_request_event_session_request(
 fn dwallet_dkg_session_request(
     deserialized_event: DWalletSessionEvent<DWalletDKGRequestEvent>,
     pulled: bool,
+) -> DwalletMPCResult<DWalletSessionRequest> {
+    Ok(DWalletSessionRequest {
+        session_type: deserialized_event.session_type,
+        session_identifier: deserialized_event.session_identifier_digest(),
+        session_sequence_number: deserialized_event.session_sequence_number,
+        protocol_data: dwallet_dkg_protocol_data(
+            deserialized_event.event_data.clone(),
+            deserialized_event.event_data.user_secret_key_share,
+        )?,
+        epoch: deserialized_event.epoch,
+        requires_network_key_data: true,
+        requires_next_active_committee: false,
+        pulled,
+    })
+}
+
+fn dwallet_dkg_with_sign_session_request(
+    deserialized_event: DWalletSessionEvent<DWalletDKGRequestEvent>,
+    pulled: bool,
+    sign_during_dkg_request: &SignDuringDKGRequestEvent,
 ) -> DwalletMPCResult<DWalletSessionRequest> {
     Ok(DWalletSessionRequest {
         session_type: deserialized_event.session_type,
