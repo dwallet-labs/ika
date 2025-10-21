d6e01128d5d1b6f6962e9b73f8b602740368c65c
Yael Abergel <yaelab200@gmail.com>
2025-10-21 17:36:32 +0300

    --wip-- [skip ci]

--- sdk/typescript/src/generated/ika_common/system_object_cap.ts
+++
@@ -1,9 +0,0 @@
-/**************************************************************
- * THIS FILE IS GENERATED AND SHOULD NOT BE MANUALLY MODIFIED *
- **************************************************************/
-import { MoveStruct } from '../utils/index.js';
-import * as object from './deps/sui/object.js';
-const $moduleName = '@local-pkg/common::system_object_cap';
-export const SystemObjectCap = new MoveStruct({ name: `${$moduleName}::SystemObjectCap`, fields: {
-        id: object.UID
-    } });
