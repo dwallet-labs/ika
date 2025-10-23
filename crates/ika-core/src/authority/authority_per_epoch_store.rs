4e643cd02db0b823efba8d460878a898a0e4852c
Yehonatan Cohen Scaly <scalys7@gmail.com>
2025-10-23 16:25:15 +0300

    Apply suggestion from @ycscaly

--- crates/ika-core/src/authority/authority_per_epoch_store.rs
+++ crates/ika-core/src/authority/authority_per_epoch_store.rs
@@ -281,7 +281,6 @@ impl AuthorityPerEpochStoreTrait for AuthorityPerEpochStore {
         &self,
         last_consensus_round: Option<Round>,
     ) -> IkaResult<Option<(Round, Vec<DWalletMPCOutput>)>> {
-        // TODO(Scaly): what is this function; name; nth(1)?
         let tables = self.tables()?;
         let mut iter = tables
             .dwallet_mpc_outputs
