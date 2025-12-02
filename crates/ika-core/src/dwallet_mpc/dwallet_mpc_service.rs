44d7c267cc32c91f554eac0af7677bc1406e28ec
Itay <itaylevy134@gmail.com>
2025-12-02 16:25:38 +0200

    good

--- crates/ika-core/src/dwallet_mpc/dwallet_mpc_service.rs
+++ crates/ika-core/src/dwallet_mpc/dwallet_mpc_service.rs
@@ -116,6 +116,7 @@ impl DWalletMPCService {
             decryption_key_reconfiguration_third_round_delay,
             dwallet_mpc_metrics.clone(),
             sui_data_receivers.clone(),
+            protocol_config.clone(),
         );
 
         Self {
@@ -163,6 +164,7 @@ impl DWalletMPCService {
                 0,
                 DWalletMPCMetrics::new(&Registry::new()),
                 sui_data_receivers.clone(),
+                ProtocolConfig::get_for_min_version(),
             ),
             exit: watch::channel(()).1,
             end_of_publish: false,
