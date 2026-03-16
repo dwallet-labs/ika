c6f061d9f9515792c5c08fa4e1f7cb26a12b7f7b
iamknownasfesal <mkircal957@gmail.com>
2026-03-16 18:07:50 +0200

    CI changes

--- docker/fullnode-x/indexer/indexer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright (c) Mysten Labs, Inc.
-# SPDX-License-Identifier: BSD-3-Clause-Clear
-
-/usr/local/bin/ika-indexer --db-url ${DATABASE_URL} --rpc-client-url ${RPC_CLIENT_URL}
