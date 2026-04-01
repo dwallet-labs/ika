dfd6db47a3075dde0f68d922ab53632845d205f7
iamknownasfesal <mkircal957@gmail.com>
2026-04-01 20:07:37 +0200

    lint

--- sdk/typescript/src/index.ts
+++ sdk/typescript/src/index.ts
@@ -1,16 +1,20 @@
 // Copyright (c) dWallet Labs, Ltd.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
-/**
- * @deprecated Use `@ika.xyz/sui` instead. This package is a compatibility
- * shim that re-exports everything from `@ika.xyz/sui`.
- *
- * ```
- * // Before:
- * import { IkaClient } from '@ika.xyz/sdk';
- *
- * // After:
- * import { IkaClient } from '@ika.xyz/sui';
- * ```
- */
-export * from '@ika.xyz/sui';
+import * as CoordinatorInnerModule from './generated/ika_dwallet_2pc_mpc/coordinator_inner.js';
+import * as CoordinatorModule from './generated/ika_dwallet_2pc_mpc/coordinator.js';
+import * as SessionsManagerModule from './generated/ika_dwallet_2pc_mpc/sessions_manager.js';
+import * as SystemModule from './generated/ika_system/system.js';
+
+export * as coordinatorTransactions from './tx/coordinator.js';
+export * as systemTransactions from './tx/system.js';
+
+export * from './client/cryptography.js';
+export * from './client/ika-client.js';
+export * from './client/ika-transaction.js';
+export * from './client/network-configs.js';
+export * from './client/types.js';
+export * from './client/user-share-encryption-keys.js';
+export * from './client/utils.js';
+
+export { CoordinatorModule, CoordinatorInnerModule, SessionsManagerModule, SystemModule };
